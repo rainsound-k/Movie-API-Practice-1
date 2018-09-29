3721b4b0ecd40605f124114149acd8c12a3bd09c
rainsound-k <rainsound128@gmail.com>
2018-09-29 18:15:15 +0900

    utils 패키지에 pagination 추가 및 MovieListCreatView에 pagination 적용

--- app/movies/views/apis.py
+++ app/movies/views/apis.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
+from utils.pagination import CustomPagination
 from ..serializers import MovieSerializer
 from ..models import Movie
 
@@ -7,3 +9,16 @@ from ..models import Movie
 class MovieListCreateView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    pagination_class = CustomPagination
+
+    def get_paginated_response(self, data):
+        return Response({
+            'status': 'ok',
+            'status_message': 'Query was successful',
+            'data': {
+                'movie_count': self.queryset.count(),
+                'limit': self.request.query_params.get('limit', '20'),
+                'page_number': self.request.query_params.get('page', '1'),
+                'movies': data,
+            }
+        })
